4c978c579a049d670d9e6aafe6da352f84d319ec
vagabondtypewriter <kieferthominquiries@gmail.com>
2022-09-07 13:12:37 -0700

    Fixes style errors

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+/**
+ * Prints a message into console.
+ *
+ * @author Kiefer Thom
+ *
+ * @version 1.0
+ */
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("The walrus comes at night!");
-    }
+  public static void main(String[] args) {
+    System.out.println("The walrus comes at night!");
+  }
 }
